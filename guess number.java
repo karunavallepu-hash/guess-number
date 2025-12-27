11ece6ea9ac95b0d265bbeb5abfaea4bf9fd1824
Karuna Vallepu <karuna24vallepu06@gmail.com>
2025-12-27 09:16:55 +0530

    Update guess number.java

--- guess number.java
+++ guess number.java
@@ -6,10 +6,10 @@ public class Main {
         Random rand = new Random();
         Scanner sc = new Scanner(System.in);
 
-        int randomNumber = rand.nextInt(10) + 1;
+        int randomNumber = rand.nextInt(100) + 1;
         int guess = 0;
 
-        System.out.println("Guess a number between 1 and 10");
+        System.out.println("Guess a number between 1 and 100");
 
         while (guess != randomNumber) {
             System.out.print("Enter your guess: ");
